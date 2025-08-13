36f02fec8d1701b31693ce5f221f78b948196062
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-08-13 15:52:09 +0900

    feat: N_L 내적값비교 추가하고, specular light 로직 수정

--- light_models/blinn_phong.cc
+++ light_models/blinn_phong.cc
@@ -13,8 +13,8 @@ Color Blinn_Phong::Shade(
 {
     Color result_color;
     float intensity, N_L, N_H;
-    float diffuse, specular;
-    float result_diffuse, result_specular;
+    float diffuse = 0.0, specular = 0.0;
+    float result_diffuse = 0.0, result_specular = 0.0;
 
     Vec3 normalize_N = Vec3::Normalize(N);
     Vec3 normalize_V = Vec3::Normalize(V);
@@ -23,16 +23,20 @@ Color Blinn_Phong::Shade(
     {
         intensity = light->GetIntensity(P);
         Vec3 normalized_L = Vec3::Normalize(light->GetDirection(P));
-        Vec3 normalized_H = Vec3::Normalize(normalized_L+normalize_V);
-
-        N_L = std::max(0.0f, Vec3::Dot(normalize_N, normalized_L));
-        diffuse = material.diffuse_coef * intensity * N_L;
-
-        N_H = std::max(0.0f, Vec3::Dot(normalize_N, normalized_H));
-        specular = material.specular_coef * intensity * std::powf(N_H, material.shininess);
         
-        result_diffuse += diffuse;
-        result_specular += specular;
+        N_L = std::max(0.0f, Vec3::Dot(normalize_N, normalized_L));
+
+        if (N_L <= 0.0f)
+            continue;
+
+        result_diffuse += material.diffuse_coef * intensity * N_L;
+        
+        if (material.shininess > 0.0f && material.specular_coef > 0.0f)
+        {
+            Vec3 normalized_H = Vec3::Normalize(normalized_L+normalize_V);
+            N_H = std::max(0.0f, Vec3::Dot(normalize_N, normalized_H));
+            result_specular += material.specular_coef * intensity * std::pow(N_H, material.shininess);
+        }
     }
 
     result_color.r = vertex_color.r*(result_diffuse + material.ambient_coef*ambient_intensity.x) + result_specular;
